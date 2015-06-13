5f992220550614d827f30419e37a1cfd9b8fd068
OT Elzinga <olaf.elzinga@hva.nl>
2015-06-13 21:59:26 +0200

    als update

--- accesslogschema.py
+++ accesslogschema.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.dialects.mysql import LONGTEXT, TINYTEXT
 from sqlalchemy.orm import relationship, backref
 
-class sett():
+class als():
 	def __init__(self):
 		self.config = configparser.ConfigParser()
 		self.config.read('logniter.config')
@@ -41,7 +41,87 @@ class sett():
 
 			def __repr__(self):
 				return "<Request(request=%s, datetime=%s, isPageview=%s)" % (self.request, self.datetime, self.isPageview)
+			class Dailypageviews(Base):
+				__tablename__ = "dailypageviews_cache"
 
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				pageviews = Column(Integer)
+
+			class Weeklypageviews(Base):
+				__tablename__ = "weeklypageviews_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				pageviews = Column(Integer)
+
+			class Monthlypageviews(Base):
+				__tablename__ = "monthlypageviews_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				pageviews = Column(Integer)
+
+			class Yearlypageviews(Base):
+				__tablename__ = "yearlypageviews_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				pageviews = Column(Integer)
+
+			class DailypageviewsPerCountry(Base):
+				__tablename__ = "dailypageviewspercountry_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				countrycode = Column(String(2))
+				pageviews = Column(Integer)
+				totaldailypageviews_id = Column(Integer, ForeignKey("dailypageviews_cache.id"))
+				
+				totaldailypageviews = relationship("Dailypageviews", backref=backref("dailypageviewspercountry", order_by=id))
+
+			class WeeklypageviewsPerCountry(Base):
+				__tablename__ = "weeklypageviewspercountry_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				countrycode = Column(String(2))
+				pageviews = Column(Integer)
+				totalweeklypageviews_id = Column(Integer, ForeignKey("weeklypageviews_cache.id"))
+				
+				totalweeklypageviews = relationship("Weeklypageviews", backref=backref("weeklypageviewspercountry", order_by=id))
+
+			class MonthlypageviewsPerCountry(Base):
+				__tablename__ = "monthlypageviewspercountry_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				countrycode = Column(String(2))
+				pageviews = Column(Integer)
+				totalmonthlypageviews_id = Column(Integer, ForeignKey("monthlypageviews_cache.id"))
+				
+				totalmonthlypageviews = relationship("Monthlypageviews", backref=backref("monthlypageviewspercountry", order_by=id))
+
+			class YearlypageviewsPerCountry(Base):
+				__tablename__ = "yearlypageviewspercountry_cache"
+
+				id = Column(Integer, primary_key=True)
+				startdate = Column(DateTime)
+				enddate = Column(DateTime)
+				countrycode = Column(String(2))
+				pageviews = Column(Integer)
+				totalyearlypageviews_id = Column(Integer, ForeignKey("yearlypageviews_cache.id"))
+				
+				totalyearlypageviews = relationship("Yearlypageviews", backref=backref("yearlypageviewspercountry", order_by=id))
+
+			self.Base.metadata.create_all(engine)
 
 
 
@@ -85,86 +165,7 @@ class Request(Base):
 
 	def __repr__(self):
 		return "<Request(request=%s, datetime=%s, isPageview=%s)" % (self.request, self.datetime, self.isPageview)
+'''
 
-class Dailypageviews(Base):
-	__tablename__ = "dailypageviews_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	pageviews = Column(Integer)
-
-class Weeklypageviews(Base):
-	__tablename__ = "weeklypageviews_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	pageviews = Column(Integer)
-
-class Monthlypageviews(Base):
-	__tablename__ = "monthlypageviews_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	pageviews = Column(Integer)
-
-class Yearlypageviews(Base):
-	__tablename__ = "yearlypageviews_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	pageviews = Column(Integer)
-
-class DailypageviewsPerCountry(Base):
-	__tablename__ = "dailypageviewspercountry_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	countrycode = Column(String(2))
-	pageviews = Column(Integer)
-	totaldailypageviews_id = Column(Integer, ForeignKey("dailypageviews_cache.id"))
-	
-	totaldailypageviews = relationship("Dailypageviews", backref=backref("dailypageviewspercountry", order_by=id))
-
-class WeeklypageviewsPerCountry(Base):
-	__tablename__ = "weeklypageviewspercountry_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	countrycode = Column(String(2))
-	pageviews = Column(Integer)
-	totalweeklypageviews_id = Column(Integer, ForeignKey("weeklypageviews_cache.id"))
-	
-	totalweeklypageviews = relationship("Weeklypageviews", backref=backref("weeklypageviewspercountry", order_by=id))
-
-class MonthlypageviewsPerCountry(Base):
-	__tablename__ = "monthlypageviewspercountry_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	countrycode = Column(String(2))
-	pageviews = Column(Integer)
-	totalmonthlypageviews_id = Column(Integer, ForeignKey("monthlypageviews_cache.id"))
-	
-	totalmonthlypageviews = relationship("Monthlypageviews", backref=backref("monthlypageviewspercountry", order_by=id))
-
-class YearlypageviewsPerCountry(Base):
-	__tablename__ = "yearlypageviewspercountry_cache"
-
-	id = Column(Integer, primary_key=True)
-	startdate = Column(DateTime)
-	enddate = Column(DateTime)
-	countrycode = Column(String(2))
-	pageviews = Column(Integer)
-	totalyearlypageviews_id = Column(Integer, ForeignKey("yearlypageviews_cache.id"))
-	
-	totalyearlypageviews = relationship("Yearlypageviews", backref=backref("yearlypageviewspercountry", order_by=id))
-
-Base.metadata.create_all(engine)
+'''
 '''
