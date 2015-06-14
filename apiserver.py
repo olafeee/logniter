b2112edef3ca0720bc92bb6483107fccc54bbd25
Bas Alphenaar <bas@balph.org>
2015-06-14 16:58:34 +0200

    -Wrapped the API server into a class; -Added (non-working) method to close the API server

--- apiserver.py
+++ apiserver.py
@@ -1,36 +1,46 @@
 import bottle
-from bottle import route, request, post
+from bottle import Bottle, run, request
 from bottle.ext.sqlalchemy import SQLAlchemyPlugin
 from accesslogschema import engine, Base, Dailypageviews, DailypageviewsPerCountry
 from sqlalchemy.orm.session import sessionmaker
 from sqlalchemy.sql.expression import and_, func
 from datetime import datetime
+
+class APIServer(object):
+
+    app = Bottle()
+
+    def __init__(self):
+        self.app.install(SQLAlchemyPlugin(engine, Base.metadata, create=True))
+        run(self.app, host='localhost', port=8080, debug=True, reloader=True)
+
+    def closeServer(self):
+        self.app.close()
+
+    @app.post('/pageviewspercountry')
+    def pageviewspercountry(db):
         
-@post('/pageviewspercountry')
-def test(db):
-    
-    startdatestring = request.json['startdate']
-    enddatestring = request.json['enddate']
-    dateformat = '%d-%m-%Y'
-    
-    startdate = datetime.strptime(startdatestring, dateformat)
-    enddate = datetime.strptime(enddatestring, dateformat)
-    
-    selectedDailypageviewsPerCountry = db.query(DailypageviewsPerCountry, func.count(DailypageviewsPerCountry.pageviews)).filter(
-        and_(DailypageviewsPerCountry.startdate >= startdate,
-        DailypageviewsPerCountry.enddate <= enddate)).group_by(DailypageviewsPerCountry.countrycode)
-    
-    returnDict = {'postdata' : request.json, 'returndata' : []}
-
-    for dailyPageviewsPerCountry in selectedDailypageviewsPerCountry:
+        startdatestring = request.json['startdate']
+        enddatestring = request.json['enddate']
+        dateformat = '%d-%m-%Y'
         
-        tempDict = {'pageviews' : dailyPageviewsPerCountry[1],
-                'countrycode' : dailyPageviewsPerCountry[0].countrycode}
+        startdate = datetime.strptime(startdatestring, dateformat)
+        enddate = datetime.strptime(enddatestring, dateformat)
+        
+        selectedDailypageviewsPerCountry = db.query(DailypageviewsPerCountry, func.count(DailypageviewsPerCountry.pageviews)).filter(
+            and_(DailypageviewsPerCountry.startdate >= startdate,
+            DailypageviewsPerCountry.enddate <= enddate)).group_by(DailypageviewsPerCountry.countrycode)
+        
+        returnDict = {'postdata' : request.json, 'returndata' : []}
 
-        returnDict['returndata'].append(tempDict)
+        for dailyPageviewsPerCountry in selectedDailypageviewsPerCountry:
+            
+            tempDict = {'pageviews' : dailyPageviewsPerCountry[1],
+                    'countrycode' : dailyPageviewsPerCountry[0].countrycode}
 
-    return returnDict
+            returnDict['returndata'].append(tempDict)
+
+        return returnDict
 
 if __name__ == '__main__':
-    bottle.install(SQLAlchemyPlugin(engine, Base.metadata, create=True))
-    bottle.run(host='localhost', port=8080, debug=True, reloader=True)
+    apiserver = APIServer()
