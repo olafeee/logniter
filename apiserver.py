698b4ca1e5c8452226bd0b84a9f84337fdd9a1fc
Bas Alphenaar <bas@balph.org>
2015-06-14 16:20:39 +0200

    Completed /pageviewspercountry endpoint

--- apiserver.py
+++ apiserver.py
@@ -3,26 +3,34 @@ from bottle import route, request, post
 from bottle.ext.sqlalchemy import SQLAlchemyPlugin
 from accesslogschema import engine, Base, Dailypageviews, DailypageviewsPerCountry
 from sqlalchemy.orm.session import sessionmaker
+from sqlalchemy.sql.expression import and_, func
+from datetime import datetime
         
-@post('/test')
+@post('/pageviewspercountry')
 def test(db):
-	allDailypageviewsPerCountry = db.query(DailypageviewsPerCountry).all()
-	
-	startdate = request.json['startdate']
-	enddate = request.json['enddate']
-	
-	alldpvDict = {'data' : []}
-	
-	for dailyPageviewsPerCountry in allDailypageviewsPerCountry:
-		tempDict = {'id' : dailyPageviewsPerCountry.id,
-				'pageviews' : dailyPageviewsPerCountry.pageviews,
-				'countrycode' : dailyPageviewsPerCountry.countrycode,
-				'startdate' : 'TODO',
-				'enddate' : 'TODO'}
-		alldpvDict['data'].append(tempDict)
+    
+    startdatestring = request.json['startdate']
+    enddatestring = request.json['enddate']
+    dateformat = '%d-%m-%Y'
+    
+    startdate = datetime.strptime(startdatestring, dateformat)
+    enddate = datetime.strptime(enddatestring, dateformat)
+    
+    selectedDailypageviewsPerCountry = db.query(DailypageviewsPerCountry, func.count(DailypageviewsPerCountry.pageviews)).filter(
+        and_(DailypageviewsPerCountry.startdate >= startdate,
+        DailypageviewsPerCountry.enddate <= enddate)).group_by(DailypageviewsPerCountry.countrycode)
+    
+    returnDict = {'postdata' : request.json, 'returndata' : []}
 
-	return alldpvDict
+    for dailyPageviewsPerCountry in selectedDailypageviewsPerCountry:
+        
+        tempDict = {'pageviews' : dailyPageviewsPerCountry[1],
+                'countrycode' : dailyPageviewsPerCountry[0].countrycode}
+
+        returnDict['returndata'].append(tempDict)
+
+    return returnDict
 
 if __name__ == '__main__':
-	bottle.install(SQLAlchemyPlugin(engine, Base.metadata, create=True))
-	bottle.run(host='localhost', port=8080, debug=True, reloader=True)
+    bottle.install(SQLAlchemyPlugin(engine, Base.metadata, create=True))
+    bottle.run(host='localhost', port=8080, debug=True, reloader=True)
