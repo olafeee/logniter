6b9526923d662adef3658c2f17a6b9e365dc9108
Bas Alphenaar <bas@balph.org>
2015-06-15 16:40:22 +0200

    Added /clientstats endpoint in API server

--- apiserver.py
+++ apiserver.py
@@ -1,9 +1,9 @@
 import bottle
 from bottle import Bottle, run, request
 from bottle.ext.sqlalchemy import SQLAlchemyPlugin
-from accesslogschema import engine, Base, Dailypageviews, DailypageviewsPerCountry
+from accesslogschema import engine, Base, Dailypageviews, DailypageviewsPerCountry, Request
 from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy.sql.expression import and_, func
+from sqlalchemy.sql.expression import and_, func, between
 from datetime import datetime
 
 class APIServer(object):
@@ -44,4 +44,33 @@ class APIServer(object):
 
         return returnDict
 
-    
+    @app.post('/clientstats')
+    def clientstats(db):
+        startdatestring = request.json['startdate']
+        enddatestring = request.json['enddate']
+        dateformat = '%d-%m-%Y'
+
+        startdate = datetime.strptime(startdatestring, dateformat)
+        enddate = datetime.strptime(enddatestring, dateformat)
+
+        selectedRequestsBrowser = db.query(Request.browsername, func.count(Request.browsername)).filter_by(isPageview=True).filter(Request.datetime.between(startdate, enddate)).group_by(Request.browsername)
+        selectedRequestsPlatform = db.query(Request.platformname, func.count(Request.platformname)).filter_by(isPageview=True).filter(Request.datetime.between(startdate, enddate)).group_by(Request.platformname)
+
+        returnDict = {'postdata' : request.json, 'returndata' : {'browserstats' : [], 'platformstats' : []}}
+
+        for selectedRequestBrowser in selectedRequestsBrowser:
+            tempDict = {'browser' : selectedRequestBrowser[0],
+            'pageviews' : selectedRequestBrowser[1]}
+
+            returnDict['returndata']['browserstats'].append(tempDict)
+
+        for selectedRequestPlatform in selectedRequestsPlatform:
+            tempDict = {'platform' : selectedRequestPlatform[0],
+            'pageviews' : selectedRequestPlatform[1]}
+
+            returnDict['returndata']['platformstats'].append(tempDict)
+
+        return returnDict
+
+if __name__ == "__main__":
+    apiserver = APIServer()
